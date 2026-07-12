619e78bbbb0ca984465fff689f0ab19dd2fa755e
SandroRDS <sandror.desouza847@gmail.com>
2026-07-12 14:39:19 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <p className="text-3xl text-blue-400 font-bold">Hello!</p>;
+}
+
+export default App;
---
+++ test/example.test.ts
@@ -0,0 +1,5 @@
+import { test, expect } from "vitest";
+
+test("This thing is working?", () => {
+  expect(true).toBe(true);
+});
